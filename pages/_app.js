a32b15505bd1637e88c149e7ee1b9fd5f06e5e77
janrebolledo <janrebolledo63@gmail.com>
2021-12-28 16:28:34 -0800

    Prevent Zoom On Textarea IOS

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function App({ Component, pageProps }) {
       <Head>
         <title>Jan Rebolledo — Multifaceted Web Developer &amp; Graphic Designer</title>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <meta name="robots" content="index, follow" />
         <meta name="keywords" content="Jan Rebolledo,graphic designer,motion designer,web designer,web developer" />
         <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async="" defer="" />
